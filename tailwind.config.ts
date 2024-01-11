64e64f667dc72946af694a1ca9296d8cb421e1f9
Ygor Bravim INSIDE <ygor@insidethebox.com.br>
2024-01-11 14:46:12 -0300

    setup: initial project setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      md: "768px",
+      lg: "1280px",
+      xl: "1920px",
+    },
+    colors: {
+      mainColor: "#db2d1b",
+      inputColor: "#ffdede",
+      textColor: "#fafafa",
+      darkTextColor: "#1e1e1e",
+      secondaryTextColor: "#1e1e1e",
+      secondaryDarkTextColor: "#fafafa",
+      bgColor: "#fafafa",
+      darkBgColor: "#1e1e1e",
+      subTextColor: "#868e96",
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
