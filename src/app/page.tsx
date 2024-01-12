f0463156d519d5d7a38d8ea86fdc7b09466c0b14
Ygor Bravim INSIDE <ygor@insidethebox.com.br>
2024-01-12 04:24:18 -0300

    feat: create about me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "@/components/Footer"
 import { GetInTouchButton } from "@/components/GetInTouchButton"
 import homeImage from "../assets/images/homeImage.png"
+import aboutImage from "../assets/images/aboutImage.jpeg"
 import { Header } from "@/components/Header"
 import Image from "next/image"
 
@@ -33,6 +34,25 @@ export default function Home() {
           />
           <div className="md:hidden">
             <GetInTouchButton href="#contact" />
+          </div>
+        </section>
+        <section id="about" className="w-full flex flex-col items-center justify-center gap-6">
+          <h3 className="uppercase text-xl" >
+            About Me
+          </h3>
+          <div className="flex flex-col md:flex-row items-center text-center justify-center gap-8">
+            <Image
+              className="rounded-tr-7xl rounded-bl-7xl rounded-tl-2xl rounded-br-2xl md:w-52 md:h-52 lg:w-72 lg:h-72"
+              src={aboutImage}
+              alt="Ygor image, person picture."
+              width={288}
+              height={288}
+            />
+            <p className="max-w-xl text-justify md:text-center">
+              Web developer, passionate for applying knowledge to improve lives and make
+              them easier and more enjoyable. Student at RocketSeat, focusing on ReactJS with Typescript.
+              Portuguese native speaker, English learned speaker, 4 years living in the United States, 22 (and counting) living in Brazil.
+            </p>
           </div>
         </section>
       </main>
